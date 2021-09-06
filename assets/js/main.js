ff315245936d4691c30881b45678b6a5bf01dcf1
HatanakaShuichiro <89886920+HatanakaShuichiro@users.noreply.github.com>
2021-09-06 10:58:10 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: '2',
+          q1: 'あああ',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '2',
           // q2: 'えええ',
           // q3: 'おおお'
         },
